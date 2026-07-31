06622afc3815ac30a424d1eaa1b3c8f4b2d5c2eb
AlbusNEWTON <albusnewton29@gmail.com>
2026-07-31 20:49:24 +0100

    Disable automatic iframe shell redirect to keep clean domain URL on nordluxe.io

--- assets/js/site-shell.js
+++ assets/js/site-shell.js
@@ -1,32 +1,19 @@
 (function () {
-  var SHELL_PATH = '/html/shell.html';
+  // Automatic iframe shell redirect disabled to preserve clean URLs (e.g. nordluxe.io)
+  if (window.top !== window.self) {
+    try {
+      var pathname = window.location.pathname || '/index.html';
+      var fileName = pathname.substring(pathname.lastIndexOf('/') + 1) || 'index.html';
+      var suffix = (window.location.search || '') + (window.location.hash || '');
+      var currentPageRef = fileName + suffix;
 
-  function currentPageRef() {
-    var pathname = window.location.pathname || '/html/index.html';
-    var fileName = pathname.substring(pathname.lastIndexOf('/') + 1) || 'index.html';
-    var suffix = (window.location.search || '') + (window.location.hash || '');
-    return fileName + suffix;
-  }
-
-  function redirectToShell() {
-    if (/\/html\/shell\.html$/i.test(window.location.pathname)) return;
-    var target = currentPageRef();
-    var shellUrl = SHELL_PATH + '?page=' + encodeURIComponent(target);
-    window.location.replace(shellUrl);
-  }
-
-  if (window.top === window.self) {
-    redirectToShell();
-    return;
-  }
-
-  try {
-    window.parent.postMessage({
-      type: 'nordluxe:navigation',
-      page: currentPageRef(),
-      title: document.title || 'NORDLUXE'
-    }, window.location.origin);
-  } catch (err) {
-    // Ignore cross-context messaging errors.
+      window.parent.postMessage({
+        type: 'nordluxe:navigation',
+        page: currentPageRef,
+        title: document.title || 'NORDLUXE'
+      }, window.location.origin);
+    } catch (err) {
+      // Ignore cross-context messaging errors.
+    }
   }
 })();
